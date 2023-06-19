26904196c57bf30d09278a3e97f824f0e98e8186
sanjay <89508377+sanjaysingh24@users.noreply.github.com>
2023-06-19 18:55:47 +0530

    Add files via upload

---
+++ js/owl-carousel.js
@@ -0,0 +1,66 @@
+
+$(function() {
+// Owl Carousel
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+ loop:true,
+items:5,
+margin: 20,
+// rewindNav:true,
+// infinte:true,
+ autoplay:true,
+  autoplaySpeed:500,
+nav: true,
+responsiveClass:true,
+
+responsive:{
+300:{
+  items:1,
+  nav:false
+},
+768:{
+  items:4
+},
+991:{
+  items:5
+}
+}
+
+
+
+});
+
+
+});
+
+// tiny-slider
+  
+var slider = tns({
+
+
+  container: ".my-slider",
+  items: 4
+  ,
+  gutter: 20,
+  slideBy: 1,
+  controlsPosition: "bottom",
+  navPosition: "bottom",
+  mouseDrag: true,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  controlsContainer: "#custom-control",
+  responsive: {
+    0: {
+      items: 1,
+      nav: false
+    },
+    768: {
+      items: 4,
+      nav: true
+    },
+    1440: {
+      items: 4
+    }
+  }
+
+});
